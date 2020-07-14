4e627d37331388570fd261958d7ad040947245ee
helther <kaeldevop@gmail.com>
2020-07-14 22:30:13 +0300

    started sqlclass

--- src/dbhelper.cpp
+++ src/dbhelper.cpp
@@ -1,4 +1,26 @@
 #include "dbhelper.h"
 #include <parserConsts.h>
 
-dbHelper::dbHelper() {}
+
+DBHelper::DBHelper()
+{
+  dbConn = QSqlDatabase::addDatabase(dbDriverName, dbPath + connName);
+  qDebug() << "constr start" << dbConn.isOpen();
+  if (!initConnection())
+    throw std::runtime_error(QString("DataBase init error: ").toStdString()
+                             + dbConn.lastError().text().toStdString());
+}
+
+DBHelper::~DBHelper()
+{
+  qDebug() << "destr start" << dbConn.isOpen();
+  dbConn.close();
+  // QSqlDatabase::removeDatabase(connName);
+  qDebug() << "destr start" << dbConn.isOpen();
+}
+
+bool DBHelper::initConnection()
+{
+  dbConn.setDatabaseName(dbName);
+  return dbConn.isValid() && dbConn.open();
+}
--- src/dbhelper.h
+++ src/dbhelper.h
@@ -1,11 +1,29 @@
 #ifndef DBHELPER_H
 #define DBHELPER_H
+#include <QtCore>
 #include <QtSql>
+// class QSqlDatabase;
 
-class dbHelper
+class DBHelper
 {
 public:
-  dbHelper();
+  DBHelper();
+  ~DBHelper();
+
+  /* no copying*/
+  DBHelper(const DBHelper &);
+  DBHelper &operator=(const DBHelper &);
+
+  QSqlDatabase getConnection() { return dbConn; }
+
+private:
+  bool initConnection();
+
+  QSqlDatabase dbConn;
+  const QString dbDriverName = "QSQLITE";
+  const QString connName = "dbConn";
+  const QString dbName = "CMM.db3";
+  const QString dbPath = "./";/// todo temp
 };
 
 #endif// DBHELPER_H
